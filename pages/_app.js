13a939d94359663d5dddd943db5f383dae3ecf0f
Diogozura <65250673+Diogozura@users.noreply.github.com>
2021-07-12 17:09:36 -0300

    primeiro commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,31 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  /* Reset CSS */
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
-`
 
+  body {
+    margin:0;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    background-color: #D9E6F6;
+  }
+#_next {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+img{
+  max-width: 100%;
+  height: auto;
+  display: block;
+}
+
+${AlurakutStyles}
+
+`
 const theme = {
   colors: {
     primary: '#0070f3',
